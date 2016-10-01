21481a4bf6492f9795d719540de2b91cf42b249c
Matthew <buccim2@gene.com>
2016-10-01 00:39:48 -0700

    cleanup parser main invocation

--- src/main.go
+++ src/main.go
@@ -10,43 +10,41 @@ import (
 	models "github.com/AlienStream/Shared-Go/models"
 )
 
-// TODO: select sources by type, then put each queue on it's own thread
 func main() {
+	source_types := []string{
+		"reddit",
+		"soundcloud",
+	}
+
+	update_interval := time.Minute * 15
 
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey: "14d6cbfd67182668a12d97372c33f13e",
 	})
 
-	updateALLSources()
+	for _, source_type := range source_types {
+		// select sources by type, then put each queue on it's own thread
+		go func(source_type string) {
+			rows, _, err := db.Con.Query("select * from sources where type LIKE %s", "%"+source_type+"%")
+			if err != nil {
+				panic(err)
+			}
 
-	longinterval := time.NewTicker(time.Minute * 15).C
-	for {
-		select {
-		case <-longinterval:
-			updateALLSources()
-			break
-		}
+			sources := models.RowsToSources(rows)
+			for _, source := range sources {
+				parser.Update(source) // Refresh automatically on boot
+			}
+
+			refreshinterval := time.NewTicker(update_interval).C
+			for {
+				select {
+				case <-refreshinterval:
+					for _, source := range sources {
+						parser.Update(source)
+					}
+					break
+				}
+			}
+		}()
 	}
-
-}
-
-func updateALLSources() {
-	var sources []models.Source = models.AllSources()
-	for _, source := range sources {
-		parser.Update(source)
-	}
-
-}
-
-func updateExpiredSources(refresh_freq int) {
-	rows, _, err := db.Con.Query("select * from sources where refresh_frequency >= %d", refresh_freq)
-	if err != nil {
-		panic(err)
-	}
-
-	var sources []models.Source = models.RowsToSources(rows)
-	for _, source := range sources {
-		go parser.Update(source)
-	}
-
 }
